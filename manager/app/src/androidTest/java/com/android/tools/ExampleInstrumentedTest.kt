9968f21ce9df1804169ae91cb28e6f21794d5704
“cduang” <2274259909@qq.com>
2023-02-10 16:43:11 +0800

    diy

--- manager/app/src/androidTest/java/com/android/tools/ExampleInstrumentedTest.kt
+++ manager/app/src/androidTest/java/com/android/tools/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package me.weishu.kernelsu
+package com.android.tools
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("me.weishu.kernelsu", appContext.packageName)
+        assertEquals("com.android.tools", appContext.packageName)
     }
 }
